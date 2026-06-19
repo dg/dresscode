730c183d5093ecbe51845f316519c090e5b25ed9
David Grudl <david@grudl.com>
2026-06-19 08:43:08 +0200

    cli: bin/dresscode with check and fix

---
+++ src/Console/exceptions.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+/**
+ * This file is part of the DressCode, a coding style and upgrade tool for PHP (https://dresscode.run)
+ * Copyright (c) 2026 David Grudl (https://davidgrudl.com)
+ */
+
+namespace DressCode\Console;
+
+
+/**
+ * The command line is invalid.
+ */
+final class UsageException extends \Exception
+{
+}
