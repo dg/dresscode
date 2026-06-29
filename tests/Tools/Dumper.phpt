7569e50713f8af3841dfe675b32429eda988cd71
David Grudl <david@grudl.com>
2026-06-29 18:53:08 +0200

    PhpSyntax: Dumper rendering nodes, slots, tokens and trivia

---
+++ tests/Tools/Dumper.phpt
@@ -0,0 +1,64 @@
+<?php declare(strict_types=1);
+
+use DressCode\Tools\Dumper;
+use PhpSyntax\Nodes\Expression\TernaryNode;
+use PhpSyntax\Nodes\Expression\VariableNode;
+use PhpSyntax\Nodes\ModifiersNode;
+use PhpSyntax\Nodes\NodeList;
+use PhpSyntax\Nodes\SeparatedNodeList;
+use PhpSyntax\Parser\Parser;
+use PhpSyntax\Token;
+use PhpSyntax\TokenKind;
+use PhpSyntax\Trivia;
+use PhpSyntax\TriviaKind;
+use Tester\Assert;
+
+require __DIR__ . '/../bootstrap.php';
+
+
+test('nodes with slots, lists and tokens with trivia', function () {
+	$a = new Token(TokenKind::Variable, '$a');
+	$a->leadingTrivia = [new Trivia(TriviaKind::OpenTag, "<?php\n"), new Trivia(TriviaKind::Whitespace, "\t")];
+	$a->trailingTrivia = [new Trivia(TriviaKind::Whitespace, ' ', inInterpolation: true)];
+	$ternary = new TernaryNode(
+		new VariableNode(null, null, $a, null),
+		new Token(ord('?'), '?'),
+		null,
+		new Token(ord(':'), ':'),
+		new VariableNode(null, null, new Token(TokenKind::Variable, '$b'), null),
+	);
+	Assert::match(<<<'XX'
+		TernaryNode
+		  cond: VariableNode
+		    name: Variable "$a"  <OpenTag"<?php\n" Whitespace"\t"  >Whitespace*" "
+		  question: '?' "?"
+		  colon: ':' ":"
+		  else: VariableNode
+		    name: Variable "$b"
+
+		XX, Dumper::dump($ternary));
+
+	$list = new SeparatedNodeList([new ModifiersNode([new Token(TokenKind::Public, 'public')]), new ModifiersNode], [new Token(ord(','), ',')]);
+	Assert::match(<<<'XX'
+		SeparatedNodeList
+		  - ModifiersNode
+		    - Public "public"
+		  - ',' ","
+		  - ModifiersNode
+
+		XX, Dumper::dump($list));
+	Assert::same("NodeList\n", Dumper::dump(new NodeList));
+});
+
+
+test('generic tree from the parser', function () {
+	Assert::match(<<<'XX'
+		GenericNode
+		  - GenericNode
+		    - GenericNode
+		      - Variable "$a"  <OpenTag"<?php "
+		      - ';' ";"
+		  - EndOfFile ""
+
+		XX, Dumper::dump((new Parser)->parse('<?php $a;')));
+});
---
+++ tools/Dumper.php
@@ -0,0 +1,81 @@
+<?php declare(strict_types=1);
+
+namespace DressCode\Tools;
+
+use PhpSyntax\Node;
+use PhpSyntax\Nodes\ModifiersNode;
+use PhpSyntax\Nodes\NodeList;
+use PhpSyntax\Nodes\SeparatedNodeList;
+use PhpSyntax\Token;
+use PhpSyntax\TokenKind;
+use PhpSyntax\Trivia;
+
+
+/**
+ * Renders a tree as indented text: node classes with their slots, tokens with kind, text and trivia.
+ */
+final class Dumper
+{
+	/** @var array<int, string> */
+	private static array $kindNames;
+
+
+	public static function dump(Node|Token $node): string
+	{
+		return self::dumpNode($node, '');
+	}
+
+
+	private static function dumpNode(Node|Token $node, string $indent): string
+	{
+		if ($node instanceof Token) {
+			return self::dumpToken($node) . "\n";
+		}
+
+		$class = $node::class;
+		$output = substr($class, strrpos($class, '\\') + 1) . "\n";
+		if (
+			$node instanceof NodeList
+			|| $node instanceof SeparatedNodeList
+			|| $node instanceof ModifiersNode
+			|| $node instanceof Parser\GenericNode
+		) {
+			foreach ($node->getChildren() as $child) {
+				$output .= "$indent  - " . self::dumpNode($child, "$indent  ");
+			}
+
+			return $output;
+		}
+
+		foreach (get_object_vars($node) as $slot => $value) {
+			if ($value instanceof Node || $value instanceof Token) {
+				$output .= "$indent  $slot: " . self::dumpNode($value, "$indent  ");
+			}
+		}
+
+		return $output;
+	}
+
+
+	private static function dumpToken(Token $token): string
+	{
+		self::$kindNames ??= array_flip(array_filter((new \ReflectionClass(TokenKind::class))->getConstants(), 'is_int'));
+		$output = (self::$kindNames[$token->kind] ?? "'$token->text'") . ' ' . self::quote($token->text);
+		foreach (['leadingTrivia' => '<', 'trailingTrivia' => '>'] as $property => $mark) {
+			if ($token->$property) {
+				$output .= '  ' . $mark . implode(' ', array_map(
+					fn(Trivia $trivia) => $trivia->kind->name . ($trivia->inInterpolation ? '*' : '') . self::quote($trivia->text),
+					$token->$property,
+				));
+			}
+		}
+
+		return $output;
+	}
+
+
+	private static function quote(string $text): string
+	{
+		return (string) json_encode($text, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+	}
+}
