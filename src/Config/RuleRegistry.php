0015064e201ae1ee3062a61a3aaea6bebb29bef6
David Grudl <david@grudl.com>
2026-07-02 15:58:51 +0200

    added rule no-trailing-whitespace
    Removes whitespace at the end of lines; the content of strings is left alone.

--- src/Config/RuleRegistry.php
+++ src/Config/RuleRegistry.php
@@ -26,6 +26,7 @@ final class RuleRegistry
 		Rules\Files\FullOpeningTagRule::class,
 		Rules\Files\LineEndingRule::class,
 		Rules\Files\NoClosingTagRule::class,
+		Rules\Files\NoTrailingWhitespaceRule::class,
 		Rules\Files\EofNewlineRule::class,
 	];
 
