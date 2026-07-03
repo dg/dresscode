002205ea8f2e17f04a905f1f72759d0ec8f4f692
David Grudl <david@grudl.com>
2026-07-03 16:20:03 +0200

    added rule elseif-keyword
    Replaces else if with elseif.

--- src/Config/RuleRegistry.php
+++ src/Config/RuleRegistry.php
@@ -22,6 +22,7 @@ final class RuleRegistry
 	private const Vendor = 'dresscode/';
 
 	private const BuiltinRules = [
+		Rules\ControlFlow\ElseifKeywordRule::class,
 		Rules\ControlFlow\NoEmptyStatementRule::class,
 		Rules\Files\NoBomRule::class,
 		Rules\Files\NoInvisibleCharactersRule::class,
