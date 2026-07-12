87b0ac868892ea6769c2c3d7373ecaee8bd701bb
David Grudl <david@grudl.com>
2026-07-12 21:53:49 +0200

    added rule control-structure-braces

--- src/Config/RuleRegistry.php
+++ src/Config/RuleRegistry.php
@@ -19,6 +19,7 @@ final class RuleRegistry
 {
 	private const BuiltinRules = [
 		Rules\ControlFlow\ContinuationPositionRule::class,
+		Rules\ControlFlow\ControlStructureBracesRule::class,
 		Rules\Files\NoBomRule::class,
 		Rules\Files\FullOpeningTagRule::class,
 		Rules\Files\LineEndingRule::class,
