0045059bacbcd549ad5f3c3b267b3db96dce9483
David Grudl <david@grudl.com>
2026-07-13 09:40:07 +0200

    added rule reference-spacing
    Removes whitespace between & and its operand.

--- src/Config/RuleRegistry.php
+++ src/Config/RuleRegistry.php
@@ -45,6 +45,7 @@ final class RuleRegistry
 		Rules\Expressions\DoubleColonSpacingRule::class,
 		Rules\Expressions\NotEqualsOperatorRule::class,
 		Rules\Expressions\ObjectOperatorSpacingRule::class,
+		Rules\Expressions\ReferenceSpacingRule::class,
 		Rules\Expressions\TernaryOperatorSpacingRule::class,
 		Rules\Expressions\UnaryOperatorSpacingRule::class,
 		Rules\PhpDoc\NoEmptyPhpDocRule::class,
