bd62fd5f4bf0433f37b2d79594974d3eec2a266a
David Grudl <david@grudl.com>
2026-07-21 11:41:50 +0200

    added rule explicit-operator-precedence

--- src/Config/RuleRegistry.php
+++ src/Config/RuleRegistry.php
@@ -58,6 +58,7 @@ final class RuleRegistry
 		Rules\Expressions\CastSpacingRule::class,
 		Rules\Expressions\ConcatSpacingRule::class,
 		Rules\Expressions\DoubleColonSpacingRule::class,
+		Rules\Expressions\ExplicitOperatorPrecedenceRule::class,
 		Rules\Expressions\NotEqualsOperatorRule::class,
 		Rules\Expressions\ObjectOperatorSpacingRule::class,
 		Rules\Expressions\MultiLineChainRule::class,
