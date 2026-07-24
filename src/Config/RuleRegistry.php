00800348c39ce891504f8739487c10df6f943e43
David Grudl <david@grudl.com>
2026-07-24 09:39:12 +0200

    added rule short-ternary-operator
    Writes $a ?: $b where a ternary repeats its condition.

--- src/Config/RuleRegistry.php
+++ src/Config/RuleRegistry.php
@@ -72,6 +72,7 @@ final class RuleRegistry
 		Rules\Expressions\SymbolicLogicalOperatorsRule::class,
 		Rules\Expressions\TernaryOperatorSpacingRule::class,
 		Rules\Expressions\MultiLineTernaryRule::class,
+		Rules\Expressions\ShortTernaryOperatorRule::class,
 		Rules\Expressions\UnaryOperatorSpacingRule::class,
 		Rules\Expressions\UselessTernaryOperatorRule::class,
 		Rules\PhpDoc\NoEmptyPhpDocRule::class,
