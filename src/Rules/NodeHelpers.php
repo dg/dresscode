69b74d953c5b5fdfa575fbedf5a794b2e53a128b
David Grudl <david@grudl.com>
2026-07-27 12:31:07 +0200

    added rule useless-else

--- src/Rules/NodeHelpers.php
+++ src/Rules/NodeHelpers.php
@@ -6,10 +6,11 @@ use PhpSyntax\Nodes\Expression;
 use PhpSyntax\Nodes\ExpressionNode;
 use PhpSyntax\Nodes\OperatorNode;
 use PhpSyntax\Nodes\Scalar;
+use PhpSyntax\Nodes\Statement;
 use PhpSyntax\Parser;
 use PhpSyntax\Token;
 use PhpSyntax\TokenKind;
-use function assert;
+use function assert, count;
 
 
 /**
@@ -132,5 +133,24 @@ final class NodeHelpers
 		$copy = clone $expression;
 		$copy->setEdgeTrivia([], []);
 		return $copy;
+	}
+
+
+	/**
+	 * Whether the block ends with a statement after which the code does not go on: return, break, continue,
+	 * goto, throw or exit.
+	 */
+	public static function endsWithExit(Statement\BlockNode $block): bool
+	{
+		$stmts = $block->statements->getItems();
+		$last = $stmts === [] ? null : $stmts[count($stmts) - 1];
+		return match (true) {
+			$last instanceof Statement\ReturnNode,
+			$last instanceof Statement\BreakNode,
+			$last instanceof Statement\ContinueNode,
+			$last instanceof Statement\GotoNode => true,
+			$last instanceof Statement\ExpressionStatementNode => $last->expression instanceof Expression\ThrowNode || $last->expression instanceof Expression\ExitNode,
+			default => false,
+		};
 	}
 }
