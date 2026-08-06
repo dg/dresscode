9b8e16ecbadf4e61ae6cded7f0fee5891caaf5ad
David Grudl <david@grudl.com>
2026-08-06 21:44:13 +0200

    added rule short-ternary-operator

--- src/Config/RuleRegistry.php
+++ src/Config/RuleRegistry.php
@@ -96,6 +96,7 @@ final class RuleRegistry
 		Rules\Expressions\StrictComparisonRule::class,
 		Rules\Expressions\SymbolicLogicalOperatorsRule::class,
 		Rules\Expressions\TernaryOperatorSpacingRule::class,
+		Rules\Expressions\ShortTernaryOperatorRule::class,
 		Rules\Expressions\UnaryOperatorSpacingRule::class,
 		Rules\Expressions\UselessTernaryOperatorRule::class,
 		Rules\PhpDoc\AnnotationNameRule::class,
