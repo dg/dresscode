53067f98d5795269f5cbf7cd63e2206937cc6420
David Grudl <david@grudl.com>
2026-08-06 19:57:49 +0200

    added rule explicit-assertion

--- src/Config/RuleRegistry.php
+++ src/Config/RuleRegistry.php
@@ -116,6 +116,7 @@ final class RuleRegistry
 		Rules\Literals\MagicConstantCasingRule::class,
 		Rules\Literals\NoTrailingWhitespaceInStringRule::class,
 		Rules\Literals\SingleQuotedStringsRule::class,
+		Rules\PhpDoc\ExplicitAssertionRule::class,
 		Rules\Types\NullableTypeForDefaultNullRule::class,
 		Rules\Types\TypeHintSpacingRule::class,
 		Rules\Variables\CombinedIssetsRule::class,
