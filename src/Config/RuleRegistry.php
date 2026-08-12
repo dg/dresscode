d2335744030aeeaed4164faf7a83468a2b646cf6
David Grudl <david@grudl.com>
2026-08-12 15:11:16 +0200

    added rule useless-alias

--- src/Config/RuleRegistry.php
+++ src/Config/RuleRegistry.php
@@ -91,6 +91,7 @@ final class RuleRegistry
 		Rules\Functions\StaticClosureRule::class,
 		Rules\Functions\StrictCallRule::class,
 		Rules\Namespaces\NoLeadingBackslashInImportRule::class,
+		Rules\Namespaces\UselessAliasRule::class,
 		Rules\Literals\ConstantCasingRule::class,
 		Rules\Literals\KeywordCasingRule::class,
 		Rules\Literals\NumericLiteralSeparatorRule::class,
