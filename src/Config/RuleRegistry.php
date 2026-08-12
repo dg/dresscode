f9f358b7bb2ec9c43f49540bee346daba0ce15a0
David Grudl <david@grudl.com>
2026-08-12 11:29:25 +0200

    added rule optimized-call-notation

--- src/Config/RuleRegistry.php
+++ src/Config/RuleRegistry.php
@@ -86,6 +86,7 @@ final class RuleRegistry
 		Rules\Functions\NoInnerFunctionsRule::class,
 		Rules\Functions\FunctionNameSpacingRule::class,
 		Rules\Functions\UselessParameterDefaultRule::class,
+		Rules\Functions\OptimizedCallNotationRule::class,
 		Rules\Functions\NoSettypeRule::class,
 		Rules\Functions\StaticClosureRule::class,
 		Rules\Functions\StrictCallRule::class,
