5045e5542da8fd6a60a67ee5318fdac740ceff21
David Grudl <david@grudl.com>
2026-08-13 13:14:03 +0200

    added rule unused-imports

--- src/Config/RuleRegistry.php
+++ src/Config/RuleRegistry.php
@@ -91,6 +91,7 @@ final class RuleRegistry
 		Rules\Functions\StaticClosureRule::class,
 		Rules\Functions\StrictCallRule::class,
 		Rules\Namespaces\NoLeadingBackslashInImportRule::class,
+		Rules\Namespaces\UnusedImportsRule::class,
 		Rules\Namespaces\UseFromSameNamespaceRule::class,
 		Rules\Namespaces\UselessAliasRule::class,
 		Rules\Literals\ConstantCasingRule::class,
