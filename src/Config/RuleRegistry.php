01486496c09919c539423359d93b61484b1a543b
David Grudl <david@grudl.com>
2026-08-17 15:36:01 +0200

    added rule no-invisible-characters

--- src/Config/RuleRegistry.php
+++ src/Config/RuleRegistry.php
@@ -64,6 +64,7 @@ final class RuleRegistry
 		Rules\ControlFlow\NoContinueInSwitchRule::class,
 		Rules\ControlFlow\UselessIfConditionWithReturnRule::class,
 		Rules\Files\NoBomRule::class,
+		Rules\Files\NoInvisibleCharactersRule::class,
 		Rules\Files\FullOpeningTagRule::class,
 		Rules\Files\LineEndingRule::class,
 		Rules\Files\NoClosingTagRule::class,
