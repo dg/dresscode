6620c04e024cc49ec6b7f3efeb8869d641922c97
David Grudl <david@grudl.com>
2026-08-18 18:37:25 +0200

    added rule static-set-state-required
    Declares __set_state() static, which is how PHP calls it.

--- src/Config/RuleRegistry.php
+++ src/Config/RuleRegistry.php
@@ -47,6 +47,7 @@ final class RuleRegistry
 		Rules\Classes\PromotedPropertyForAssignedParameterRule::class,
 		Rules\Classes\SelfForCurrentClassRule::class,
 		Rules\Classes\StringableRequiredRule::class,
+		Rules\Classes\StaticSetStateRequiredRule::class,
 		Rules\Classes\SingleMemberPerDeclarationRule::class,
 		Rules\Classes\SingleMemberPerLineRule::class,
 		Rules\Classes\UselessModifierRule::class,
