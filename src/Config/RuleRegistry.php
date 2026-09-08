e7a1c22a656beeca84ee8e8acf991327e6dd8474
David Grudl <david@grudl.com>
2026-09-08 17:32:11 +0200

    added rule json-validate-for-decode-test
    Replaces json_decode() called only to test whether the text is JSON with json_validate().

--- src/Config/RuleRegistry.php
+++ src/Config/RuleRegistry.php
@@ -95,6 +95,7 @@ final class RuleRegistry
 		Rules\Functions\OptimizedCallNotationRule::class,
 		Rules\Functions\NoSettypeRule::class,
 		Rules\Functions\NoManualSubstringTestRule::class,
+		Rules\Functions\JsonValidateForDecodeTestRule::class,
 		Rules\Functions\UselessSetAccessibleRule::class,
 		Rules\Functions\StaticClosureRule::class,
 		Rules\Functions\StrictCallRule::class,
