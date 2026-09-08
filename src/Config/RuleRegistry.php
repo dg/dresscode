5460c1db3a6d977ca4742d7e899c49ceedf058ac
David Grudl <david@grudl.com>
2026-09-08 16:09:40 +0200

    added rule first-class-callable-notation
    Writes Closure::fromCallable() and a closure that only passes its parameters on to one call as a first-class callable, strlen(...).

--- src/Config/RuleRegistry.php
+++ src/Config/RuleRegistry.php
@@ -79,6 +79,7 @@ final class RuleRegistry
 		Rules\Files\NoClosingTagRule::class,
 		Rules\Files\StrictTypesRequiredRule::class,
 		Rules\Functions\ArrowFunctionRule::class,
+		Rules\Functions\FirstClassCallableNotationRule::class,
 		Rules\Functions\ForbiddenFunctionsRule::class,
 		Rules\Functions\MultiLineCallRule::class,
 		Rules\Functions\NoDirnameOfFileRule::class,
